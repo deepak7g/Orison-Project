4c13b1c9035fe84391fc1712d467e873c634513b
deepak7g <push.default>
2024-09-03 14:09:38 +0530

    Final Project

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class storeDB(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.CharField(max_length=1000)
+    messaged_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import storeDB
 # Create your views here.
+
+def home(request):
+    print("Recived Save Request!...")
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        #print("name", name)
+        #print("email", email)
+        #print("message", message)
+        #print(request.POST)
+        user = storeDB.objects.create(name=name, email=email, message=message)
+        user.save()
+        if user is not None:
+            messages.success(request, "Data Stored....")
+            return redirect('home')
+            #return HttpResponse("Your data has been saved!")
+    
+    return render(request, 'home.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     if request.method == "POST":
+#         name = request.POST.get("name")
+#         email = request.POST.get("email")
+#         message = request.POST.get("textarea")
+        
+#         if name and email and message:
+#             storeDB.objects.create(name=name, email=email, message=message)
+#             return HttpResponse('Your message has been sent!')
+            # storeDB.objects.create(name=name, email=email, message=message)
+            # messages.success(request,  'Succesfully Submitted')
+            # return redirect('home')
+        # else:
+        #     messages.success(request,  'not Submitted')
+        #     return redirect('home')
